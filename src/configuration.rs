cb4c61b1d97096fec1604605584dea09e4957c95
David Weis <davidmichaelweis@gmail.com>
2022-06-26 23:22:21 +0100

    Setup configuration and dependencies

---
+++ src/configuration.rs
@@ -0,0 +1,64 @@
+use log::*;
+use serde::Deserialize;
+use std::{path::PathBuf, str};
+
+/// Use default config if no path is provided
+pub fn get_configuration(config: Option<PathBuf>) -> Result<AppConfig, anyhow::Error> {
+    let mut settings = config::Config::default();
+
+    if let Some(config) = config {
+        info!("Using configuration from {:?}", config);
+        settings.merge(config::File::with_name(
+            config
+                .to_str()
+                .ok_or_else(|| anyhow::anyhow!("Failed to convert path"))?,
+        ))?;
+    } else {
+        info!("Using dev configuration");
+        settings
+            .merge(config::File::with_name("configuration/settings"))?
+            .merge(config::File::with_name("configuration/dev_settings"))?;
+    }
+
+    settings.merge(config::Environment::with_prefix("APP"))?;
+
+    Ok(settings.try_into()?)
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct AppConfig {
+    pub server_config: ServerConfig,
+    pub alarm_config: AlarmConfig,
+    pub mqtt: MqttConfig,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: u16,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+pub struct AlarmConfig {
+    pub save_file_path: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+pub struct BlindsConfig {
+    pub url: String,
+}
+
+// weird serde default thing
+const DEFAULT_MQTT_PORT: u16 = 1883;
+
+const fn default_mqtt_port() -> u16 {
+    DEFAULT_MQTT_PORT
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct MqttConfig {
+    pub broker_host: String,
+    #[serde(default = "default_mqtt_port")]
+    pub broker_port: u16,
+    pub client_id: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+mod configuration;
+
+use configuration::get_configuration;
+use log::*;
+use simplelog::*;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    version = "0.1.0",
+    author = "David M. Weis <dweis7@gmail.com>",
+    about = "Hub System"
+)]
+struct Opts {
+    #[structopt(long)]
+    config: Option<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    setup_logging();
+    let opts = Opts::from_args();
+    info!("Starting Hub System");
+    let _app_config = get_configuration(opts.config)?;
+
+    Ok(())
+}
+
+fn setup_logging() {
+    if TermLogger::init(
+        LevelFilter::Info,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )
+    .is_err()
+    {
+        eprintln!("Failed to create term logger");
+        if SimpleLogger::init(LevelFilter::Info, Config::default()).is_err() {
+            eprintln!("Failed to create simple logger");
+        }
+    }
 }
