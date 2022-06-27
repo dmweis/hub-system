55fc16e995588788cb18a26f05bea2d2ac0896ad
dmweis <dmweis@users.noreply.github.com>
2022-06-27 02:32:09 +0000

    deploy: 308e64ae9bc60b5628ab7bdf59b65acd6bbacf86

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["hub_system"] = {"name":"","files":["configuration.rs","ioc.rs","main.rs","mqtt_server.rs","routes.rs","speech_service.rs"]};
+sourcesIndex["hub_system"] = {"name":"","files":["blinds_service.rs","configuration.rs","ioc.rs","main.rs","mqtt_server.rs","routes.rs","speech_service.rs"]};
 createSourceSidebar();
